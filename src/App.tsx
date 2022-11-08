9a87d077a097d3708de11726a4f85550bc217435
gerasalgado <geradiler@hotmail.com>
2022-11-08 13:01:00 -0600

    load QA2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gerardo Salgado 2
+          Gerardo Salgado
         </p>
         <a
           className="App-link"
