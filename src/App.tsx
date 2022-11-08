98432e875b7614403c27c92637b24b64823cedd5
gerasalgado <geradiler@hotmail.com>
2022-11-08 10:29:38 -0600

    update5

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gerardo Salgado
+          Gerardo Salgado 2
         </p>
         <a
           className="App-link"
