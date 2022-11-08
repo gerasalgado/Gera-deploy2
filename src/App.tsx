283e36faece67c3c9e897fc1cd62f653f883150d
gerasalgado <geradiler@hotmail.com>
2022-11-07 22:15:44 -0600

    Gerardo Name

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Citlali :3
+          Gerardo Salgado
         </p>
         <a
           className="App-link"
